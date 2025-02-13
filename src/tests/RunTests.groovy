2971275ad806b29874608212e52f1fc597959861
Vinícius Ponte <vinicius.mponte@gmail.com>
2025-02-13 08:20:39 -0300

    test: Formatação da apresentação do resultado no terminal

--- src/tests/RunTests.groovy
+++ src/tests/RunTests.groovy
@@ -7,40 +7,43 @@ import managers.UserManagerTest
 
 static void main(String[] args) {
 
+    def result = []
+
+
     //Candidate Setters and Getters
-    println CandidateTest.setAndGetName() ? "passou" : "falhou"
-    println CandidateTest.setAndGetEmail() ? "passou" : "falhou"
-    println CandidateTest.setAndGetCountry() ? "passou" : "falhou"
-    println CandidateTest.setAndGetState() ? "passou" : "falhou"
-    println CandidateTest.setAndGetCep() ? "passou" : "falhou"
-    println CandidateTest.setAndGetDescription() ? "passou" : "falhou"
-    println CandidateTest.setAndGetAge() ? "passou" : "falhou"
-    println CandidateTest.setAndGetCpf() ? "passou" : "falhou"
-    println CandidateTest.setAndGetSkills() ? "passou" : "falhou"
+    result.add((CandidateTest.setAndGetName() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetName")
+    result.add((CandidateTest.setAndGetEmail() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetEmail")
+    result.add((CandidateTest.setAndGetCountry() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetCountry")
+    result.add((CandidateTest.setAndGetState() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetState")
+    result.add((CandidateTest.setAndGetCep() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetCep")
+    result.add((CandidateTest.setAndGetDescription() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetDescription")
+    result.add((CandidateTest.setAndGetAge() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetAge")
+    result.add((CandidateTest.setAndGetCpf() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetCpf")
+    result.add((CandidateTest.setAndGetSkills() ? "[PASSED]" : "[FAILED]") + " CandidateTest.setAndGetSkills")
 
     //Enterprise Setters and Getters
-    println EnterpriseTest.setAndGetName() ? "passou" : "falhou"
-    println EnterpriseTest.setAndGetEmail() ? "passou" : "falhou"
-    println EnterpriseTest.setAndGetCountry() ? "passou" : "falhou"
-    println EnterpriseTest.setAndGetState() ? "passou" : "falhou"
-    println EnterpriseTest.setAndGetCep() ? "passou" : "falhou"
-    println EnterpriseTest.setAndGetDescription() ? "passou" : "falhou"
-    println EnterpriseTest.setAndGetCnpj() ? "passou" : "falhou"
-    println EnterpriseTest.setAndGetSkills() ? "passou" : "falhou"
+    result.add((EnterpriseTest.setAndGetName() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetName")
+    result.add((EnterpriseTest.setAndGetEmail() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetEmail")
+    result.add((EnterpriseTest.setAndGetCountry() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetCountry")
+    result.add((EnterpriseTest.setAndGetState() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetState")
+    result.add((EnterpriseTest.setAndGetCep() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetCep")
+    result.add((EnterpriseTest.setAndGetDescription() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetDescription")
+    result.add((EnterpriseTest.setAndGetCnpj() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetCnpj")
+    result.add((EnterpriseTest.setAndGetSkills() ? "[PASSED]" : "[FAILED]") + " EnterpriseTest.setAndGetSkills")
 
 
     //UserManager - register
-    println UserManagerTest.successfulRegisterEnterprise() ? "passou" : "falhou"
-    println UserManagerTest.validateSkillsRegisterEnterprise() ? "passou" : "falhou"
-    println UserManagerTest.successfulRegisterCandidate() ? "passou" : "falhou"
-    println UserManagerTest.validateSkillsRegisterCandidate() ? "passou" : "falhou"
+    result.add((UserManagerTest.successfulRegisterEnterprise() ? "[PASSED]" : "[FAILED]") + " UserManagerTest.successfulRegisterEnterprise")
+    result.add((UserManagerTest.validateSkillsRegisterEnterprise() ? "[PASSED]" : "[FAILED]") + " UserManagerTest.validateSkillsRegisterEnterprise")
+    result.add((UserManagerTest.successfulRegisterCandidate() ? "[PASSED]" : "[FAILED]") + " UserManagerTest.successfulRegisterCandidate")
+    result.add((UserManagerTest.validateSkillsRegisterCandidate() ? "[PASSED]" : "[FAILED]") + " UserManagerTest.validateSkillsRegisterCandidate")
 
     //UserManager - remove
-    println UserManagerTest.successfulRemoveCandidate() ? "passou" : "falhou"
-    println UserManagerTest.successfulRemoveEnterprise() ? "passou" : "falhou"
+    result.add((UserManagerTest.successfulRemoveCandidate() ? "[PASSED]" : "[FAILED]") + " UserManagerTest.successfulRemoveCandidate")
+    result.add((UserManagerTest.successfulRemoveEnterprise() ? "[PASSED]" : "[FAILED]") + " UserManagerTest.successfulRemoveEnterprise")
 
 
-
-    //print "\033[H\033[2J"
+    println "\033[H\033[2J"
+    println result.join("\n")
 }
 
