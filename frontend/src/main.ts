19f4c9ccca6568b22acceead6f09a05484092a54
Vinícius Ponte <vinicius.mponte@gmail.com>
2025-02-20 05:05:38 -0300

    chore: configuração de ambiente para desenvolvimento frontend com TypeScript

---
+++ frontend/src/main.ts
@@ -0,0 +1,8 @@
+import {MyClass} from './test/MyClass';
+
+const test: string = 'Hello World with TypeScript';
+console.log(test)
+
+const myClassInstance = new MyClass();
+myClassInstance.printMessage();
+
---
+++ frontend/src/test/MyClass.ts
@@ -0,0 +1,5 @@
+export class MyClass {
+    printMessage(): void {
+        console.log('Hello from MyClass');
+    }
+}
