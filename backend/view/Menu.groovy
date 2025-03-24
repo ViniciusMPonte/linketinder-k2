4b61982ace32c8fedc81e1731d6fa97cb685d16d
vinicius <vinicius.mponte@gmail.com>
2025-03-24 01:29:30 -0300

    feat: Adição de leitura, atualização e exclusão de candidatos via CLI.

--- backend/view/Menu.groovy
+++ backend/view/Menu.groovy
@@ -1,14 +1,9 @@
 package view
 
-import entities.Candidate
-import entities.Enterprise
 import entities.NewCandidate
 import entities.NewEnterprise
-import services.CompatibilityService
 import services.SectionService
-import utils.GenericUtils
 
-import java.util.function.Function
 
 class Menu {
 
@@ -19,38 +14,46 @@ class Menu {
     }
 
     def startMenu() {
-        println """
-        === Bem vindo ao Linketinder ===
-        1. Fazer login Empresa
-        2. Fazer login Candidato
-        3. Cadastrar Empresa
-        4. Cadastrar Candidato
-        0. Sair
-        """
-
-        print "Escolha uma opção: "
-
-        switch (this.section.input.nextLine()) {
-            case "1":
-                this.loginEnterprise()
-                break
-            case "2":
-                this.loginCandidate()
-                break
-            case "3":
-                this.registerEnterprise()
-                break
-            case "4":
-                this.registerCandidate()
-                break
-            case "0":
-                println "Saindo..."
-                return
-            default:
-                println "Opção inválida, tente novamente."
+        if (this.section.userLogged instanceof NewCandidate) {
+            this.mainMenuCandidate()
         }
+        if (this.section.userLogged instanceof NewEnterprise) {
+            println "A variável é do tipo NewEnterprise"
+        }
+        if (this.section.userLogged == null) {
+                println """
+            === Bem vindo ao Linketinder ===
+            1. Fazer login Empresa
+            2. Fazer login Candidato
+            3. Cadastrar Empresa
+            4. Cadastrar Candidato
+            0. Sair
+            """.stripIndent()
 
-        startMenu()
+            print "Escolha uma opção: "
+
+            switch (this.section.input.nextLine()) {
+                case "1":
+                    this.loginEnterprise()
+                    break
+                case "2":
+                    this.loginCandidate()
+                    break
+                case "3":
+                    this.registerEnterprise()
+                    break
+                case "4":
+                    this.registerCandidate()
+                    break
+                case "0":
+                    println "Saindo..."
+                    return
+                default:
+                    println "Opção inválida, tente novamente."
+            }
+
+        }
+        this.startMenu()
     }
 
     void loginEnterprise() {
@@ -90,7 +93,7 @@ class Menu {
         
         1. tentar novamente
         0. Voltar
-        """
+        """.stripIndent()
 
         switch (this.section.input.nextLine()) {
             case "1":
@@ -132,19 +135,19 @@ class Menu {
         println "=== Cadastrar Candidato ==="
 
         Map params = [
-                email: this.getQuestionResult("\nDigite o email: "),
-                password: this.getQuestionResult("\nDigite sua senha: "),
-                name: this.getQuestionResult("\nDigite seu nome: "),
+                email      : this.getQuestionResult("\nDigite o email: "),
+                password   : this.getQuestionResult("\nDigite sua senha: "),
+                name       : this.getQuestionResult("\nDigite seu nome: "),
                 description: this.getQuestionResult("\nDescreva-se: "),
-                cpf: this.getQuestionResult("\nDigite seu CPF: "),
-                birthday: this.getQuestionResult("\nDigite a data do seu aniversário [YYY-MM-DD]: "),
-                country: this.getQuestionResult("\nDigite o páis que mora (Brasil): "),
-                state: this.getQuestionResult("\nDigite o estado que você mora: "),
-                postalCode: this.getQuestionResult("\nDigite seu CEP: "),
-                skills: this.getQuestionResult(
+                cpf        : this.getQuestionResult("\nDigite seu CPF: "),
+                birthday   : this.getQuestionResult("\nDigite a data do seu aniversário [YYY-MM-DD]: "),
+                country    : this.getQuestionResult("\nDigite o páis que mora (Brasil): "),
+                state      : this.getQuestionResult("\nDigite o estado que você mora: "),
+                postalCode : this.getQuestionResult("\nDigite seu CEP: "),
+                skills     : this.getQuestionResult(
                         "\nDigite suas habilidades, separadas por virgula " +
                                 "[Opções: Java, Groovy, JavaScript, Git, GitHub, SQL, NoSQL, Angular, Spring, Docker]: "
-                )?.replaceAll(/[{}]/, '')?.split(',')?.toList() ?: []
+                )?.replaceAll(/ /, '')?.split(',')?.toList() ?: []
         ]
 
         if (this.section.dbManager.saveNewCandidate(new NewCandidate(params))) {
@@ -153,6 +156,107 @@ class Menu {
         }
 
         this.errorMenu("Falha ao cadastrar candidato", this.&registerCandidate)
+    }
+
+    def mainMenuCandidate() {
+        println """
+        === Menu Candidato ===
+        1. Visualizar Vagas
+        2. Perfil
+        0. Logout
+        """.stripIndent()
+
+        print "Escolha uma opção: "
+
+        switch (this.section.input.nextLine()) {
+            case "1":
+
+                break
+            case "2":
+                this.RUDMenuCandidate()
+                break
+            case "0":
+                println "Deslogando..."
+                this.section.userLogged = null
+                break
+            default:
+                println "Opção inválida, tente novamente."
+        }
+    }
+
+    def RUDMenuCandidate() {
+        println """
+        === Perfil do Candidato ===
+        1. Visualizar Perfil
+        2. Editar Perfil
+        3. Excluir Perfil
+        0. Voltar
+        """.stripIndent()
+
+        print "Escolha uma opção: "
+
+        switch (this.section.input.nextLine()) {
+            case "1":
+                this.readCandidateProfile()
+                break
+            case "2":
+                this.updateCandidate()
+                break
+            case "3":
+                this.deleteCandidate()
+                break
+            case "0":
+                println "Voltando..."
+                return
+            default:
+                println "Opção inválida, tente novamente."
+        }
+
+        if (this.section.userLogged == null) return
+        this.RUDMenuCandidate()
+    }
+
+    void readCandidateProfile() {
+        println this.section.userLogged
+    }
+
+    void updateCandidate() {
+
+        println "=== Editar Candidato ==="
+
+        Map params = [
+                email      : this.getQuestionResult("\nDigite o email: "),
+                password   : this.getQuestionResult("\nDigite sua senha: "),
+                name       : this.getQuestionResult("\nDigite seu nome: "),
+                description: this.getQuestionResult("\nDescreva-se: "),
+                cpf        : this.getQuestionResult("\nDigite seu CPF: "),
+                birthday   : this.getQuestionResult("\nDigite a data do seu aniversário [YYY-MM-DD]: "),
+                country    : this.getQuestionResult("\nDigite o páis que mora (Brasil): "),
+                state      : this.getQuestionResult("\nDigite o estado que você mora: "),
+                postalCode : this.getQuestionResult("\nDigite seu CEP: "),
+                skills     : this.getQuestionResult(
+                        "\nDigite suas habilidades, separadas por virgula " +
+                                "[Opções: Java, Groovy, JavaScript, Git, GitHub, SQL, NoSQL, Angular, Spring, Docker]: "
+                )?.replaceAll(/ /, '')?.split(',')?.toList() ?: []
+        ]
+
+        if (this.section.dbManager.updateCandidate(this.section.userLogged as NewCandidate, new NewCandidate(params))) {
+            this.section.userLogged = this.section.dbManager.getCandidateById(this.section.userLogged.getId())
+            println "\nPerfil editado com sucesso."
+            return
+        }
+
+        this.errorMenu("Falha ao tentar editar perfil do candidato.", this.&registerCandidate)
+    }
+
+    void deleteCandidate() {
+        this.section.dbManager.deleteCandidateById(this.section.userLogged.getId())
+        if (!this.section.dbManager.getCandidateById(this.section.userLogged.getId())) {
+            this.section.userLogged = null
+            println "\nCandidato Excluído com sucesso!"
+            return
+        }
+        this.errorMenu("Erro ao tentar excluir candidato", this.&deleteCandidate)
     }
 
 
@@ -169,7 +273,7 @@ class Menu {
         
         1. Tentar novamente
         0. Voltar
-        """
+        """.stripIndent()
 
         switch (this.section.input.nextLine()) {
             case "1":
